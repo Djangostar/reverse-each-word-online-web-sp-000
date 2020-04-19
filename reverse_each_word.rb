328e1afc10d7d3658749f9b6e858091e12f50196
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-19 00:46:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,5 @@ def reverse_each_word(string)
   # reverses all the words in a string without reversing the order of the words
   # reverses all the words in another string without reversing the order of the words
   # uses collect
-
-  reverse_array = []
-  reverse array << string.split.each do
-    |word| word.reverse
-    reverse_array.join(" ")
-  end
-  reverse array
-  #string.split.collect {|word| word.reverse}.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
