51400bb949e4974cf1b2a0fa4c89bd19ff5c7d0e
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-19 00:47:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,7 @@ def reverse_each_word(string)
   # reverses all the words in a string without reversing the order of the words
   # reverses all the words in another string without reversing the order of the words
   # uses collect
-  string.split.collect {|word| word.reverse}.join(" ")
+  string.split.collect do
+    |word| word.reverse
+  end.join(" ")
 end
